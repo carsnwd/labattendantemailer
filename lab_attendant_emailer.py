56960ceab1c81afadd120c74372395ae15cdca02
carsnwd <carsnwd@gmail.com>
2016-02-27 20:44:17 -0500

    Sends emails properly, needs some formatting work and refactoring of code to make it cleaner.

--- lab_attendant_emailer.py
+++ lab_attendant_emailer.py
@@ -1,6 +1,8 @@
 from tkinter import *  # GUI library
 import tkinter.messagebox
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 # @author: Carson Wood
 # Sends automated emails out for lab attendant job
@@ -12,27 +14,27 @@ class Student:
         self.item = item
 
     @property
-    def email(self):
+    def semail(self):
         return self.email
 
-    @email.setter
-    def email(self, email):
+    @semail.setter
+    def semail(self, email):
         self.email = email
 
     @property
-    def name(self):
+    def sname(self):
         return self.name
 
-    @name.setter
-    def name(self, name):
+    @sname.setter
+    def sname(self, name):
         self.name = name
 
     @property
-    def item(self):
+    def sitem(self):
         return self.item
 
-    @item.setter
-    def item(self, item):
+    @sitem.setter
+    def sitem(self, item):
         self.item = item
 
 class LabAttendant:
@@ -42,28 +44,66 @@ class LabAttendant:
         self.lab = lab
 
     @property
-    def email(self):
+    def lemail(self):
         return self.email
 
-    @email.setter
-    def email(self, email):
+    @lemail.setter
+    def lemail(self, email):
         self.email = email
 
     @property
-    def password(self):
+    def lpassword(self):
         return self.password
 
-    @password.setter
-    def password(self, password):
+    @lpassword.setter
+    def lpassword(self, password):
         self.password = password
 
     @property
-    def lab(self):
+    def llab(self):
         return self.lab
 
-    @lab.setter
-    def lab(self, password):
+    @llab.setter
+    def llab(self, password):
         self.password = password
+
+def SendEmail(labattendant, student):
+    message = MIMEMultipart()
+    message['From'] = labattendant.email
+    message['To'] = student.email
+    message['Subject'] = student.item + " left in " + labattendant.lab + " computer lab."
+    if(labattendant.lab == "GRH106"):
+        if "id" == student.item: #Left ID in Grove
+            body = "Hello " + student.name + ",\nI believe you left your " + student.item + " in the " + labattendant.lab\
+                   + " computer lab. If so, please stop by the " + labattendant.lab\
+                   + " computer lab, and ask the lab attendant to return your " + student.item + " to you.\n\n"\
+                   + "Lab attendants should be in the " + labattendant.lab + " computer lab Monday through Thursday 8AM-10PM and Friday 8AM-4PM.\n\n"+ "Thank you!\n-Lab Attendant"
+        else: #Left other item in Grove
+            body = "Hello " + student.name + ",\nI believe you left your " + student.item + " in the " + labattendant.lab\
+                   + " computer lab. If so, please stop by the " + labattendant.lab\
+                   + " computer lab, with your ID, and ask the lab " + "attendant to return your " + student.item + " to you.\n\n"\
+                   + "Lab attendants should be in the " + labattendant.lab + " computer lab Monday through Thursday 8AM-10PM and Friday 8AM-4PM.\n\n"+ "Thank you!\n-Lab Attendant"
+    else: #Left ID in MCT/CUB
+        if "id" == student.item:
+            body = "Hello " + student.name + ",\nI believe you left your " + student.item + " in the " + labattendant.lab\
+                   + " computer lab. If so, please stop by the " + labattendant.lab\
+                   + " computer lab, and ask the lab attendant to return your " + student.item +" to you."\
+                   + "\n\n" + "Lab attendants should be in the " + labattendant.lab + " computer lab Monday through Thursday 8AM-10PM, Friday 8AM-4PM, and weekends 12PM-6PM.\n\n"+ "Thank you!\n-Lab Attendant"
+        else: #Left other item in MCT/CUB
+            body = "Hello " + student.name + ",\nI believe you left your " + student.item + " in the " + labattendant.lab\
+                   + " computer lab. If so, please stop by the " + labattendant.lab\
+                   + " computer lab, with your ID, and ask the lab " + "attendant to return your " + student.item +""\
+                   + " to you.\n\n" + "Lab attendants should be in the " + labattendant.lab + " computer lab Monday through Thursday 8AM-10PM, Friday 8AM-4PM, and weekends 12PM-6PM.\n\n"+ "Thank you!\n-Lab Attendant"
+    message.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP("smtp.gmail.com")
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(labattendant.email, labattendant.password)
+    text = message.as_string()
+    server.sendmail(labattendant.email, student.email, text)
+
 
 class GUI:
     def __init__(self, master):
@@ -94,11 +134,12 @@ class GUI:
         labattendant_email_label = Label(labattendant_info_frame, text="Your email: ")
         labattendant_email_label.grid(row=3, column=0)
         labattendant_email_entry = Entry(labattendant_info_frame)
-        labattendant_email_entry.insert(END, '@ship.edu')
+        labattendant_email_entry.insert(END, 'shiplabattendant@gmail.com')
         labattendant_email_entry.grid(row=3, column=1)
         labattendant_password_label = Label(labattendant_info_frame, text="Email password: ")
         labattendant_password_label.grid(row=4, column=0)
         labattendant_password_entry = Entry(labattendant_info_frame, show='*')
+        labattendant_password_entry.insert(END, 'labsthataremicro')
         labattendant_password_entry.grid(row=4, column=1)
         labattendant_lab_label = Label(labattendant_info_frame, text="Current lab: ")
         labattendant_lab_label.grid(row=5, column=0)
@@ -108,9 +149,14 @@ class GUI:
 
         # Button handler for the submit button
         def submit_on_click():
-            if "@ship.edu" not in labattendant_email_entry.get():
-                tkinter.messagebox.showwarning("Error", "Please use your @ship.edu email.")
-                labattendant_email_entry.delete(0, 'end')
+            #if "@ship.edu" not in labattendant_email_entry.get():
+             #   tkinter.messagebox.showwarning("Error", "Please use your @ship.edu email.")
+              #  labattendant_email_entry.delete(0, 'end')
+            #else:
+                labattendant = LabAttendant(labattendant_email_entry.get(), labattendant_password_entry.get(),
+                                            labattendant_lab_menu.cget("text"))
+                student = Student(student_name_entry.get(), student_email_entry.get(), student_item_entry.get().lower())
+                SendEmail(labattendant, student)
 
 
         # Collects data and sends it
